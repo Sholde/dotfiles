e60f6078a85bdbfbcdcd67cb4f3588b8da1ed5c8
Sholde <exshode@gmail.com>
2020-10-15 21:31:27 +0200

    rework regexp for function(optimize), end change to float

--- highlight-scilab.el
+++ highlight-scilab.el
@@ -4,7 +4,7 @@
 (setq scilab-highlights
       '(
 	;; Type
-	("\\(function\\|endfunction\\)[[:blank:]\s\t\n ]"			  . font-lock-type-face)
+	("\\(^function[[:space:][:blank:]]\\|^endfunction$\\)"			  . font-lock-type-face)
 
 	;; Keyword
 	("[[:blank:]\s\t\n ]\\(for\\|while\\|if\\|then\\|else\\|elseif\\|end\\|do\\|return\\|select\\|case\\|global\\|try\\|catch\\)[[:blank:]\s\t\n ]"                         	  . font-lock-keyword-face)
@@ -16,7 +16,7 @@
 	("//.*\n"    								  . font-lock-comment-face)
 	
 	;; Constant / Number
-	("[[:blank:]\s\t\n ][0-9]+\\(\\s.[0-9]+\\)?"                    	  . font-lock-constant-face)))
+	("[[:blank:]\s\t\n ][0-9]+\\(\\.[0-9]+\\)?"                     	  . font-lock-constant-face)))
 
 ;; Handling comments // ...
 (setq scilab-mode-syntax-table

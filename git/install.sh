2c460e4150858aefc40dd7fa5c0cecb2219afacc
Sholde <exshode@gmail.com>
2021-01-21 17:22:57 +0100

    add git alias

---
+++ git/install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+git config --global alias.co checkout
+git config --global alias.br branch
+git config --global alias.ci commit
+git config --global alias.st status
--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-list_of_dir="bash emacs i3"
+list_of_dir="bash emacs i3 git"
 
 for dir in $list_of_dir ; do
     make -C $dir

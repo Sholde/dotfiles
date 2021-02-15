b39b68c4671f17f6935f93c13002f15f5df7a2df
Sholde <exshode@gmail.com>
2021-02-15 20:54:24 +0100

    fix comment

--- emacs/auto-complete-mpi.el
+++ emacs/auto-complete-mpi.el
@@ -57,30 +57,30 @@
    "MPI_Ssend_init" "MPI_Start" "MPI_Startall" "MPI_Test"
    "MPI_Test_cancelled" "MPI_Testall" "MPI_Testany" "MPI_Testsome"
    "MPI_Wait" "MPI_Waitall" "MPI_Waitany" "MPI_Waitsome"
-   ;;CollectiveCommunicationRoutines
+   ;;Collective Communication Routines
    "MPI_Allgather" "MPI_Allgatherv" "MPI_Allreduce" "MPI_Alltoall"
    "MPI_Alltoallv" "MPI_Barrier" "MPI_Bcast" "MPI_Gather"
    "MPI_Gatherv" "MPI_Op_create" "MPI_Op_free" "MPI_Reduce"
    "MPI_Reduce_scatter" "MPI_Scan" "MPI_Scatter" "MPI_Scatterv"
-   ;;ProcessGroupRoutines
+   ;;Process Group Routines
    "MPI_Group_compare" "MPI_Group_difference" "MPI_Group_excl" "MPI_Group_free"
    "MPI_Group_incl" "MPI_Group_intersection" "MPI_Group_range_excl" "MPI_Group_range_incl"
    "MPI_Group_rank" "MPI_Group_size" "MPI_Group_translate_ranks" "MPI_Group_union"
-   ;;CommunicatorsRoutines
+   ;;Communicators Routines
    "MPI_Comm_compare" "MPI_Comm_create" "MPI_Comm_dup" "MPI_Comm_free"
    "MPI_Comm_group" "MPI_Comm_rank" "MPI_Comm_remote_group" "MPI_Comm_remote_size"
    "MPI_Comm_size" "MPI_Comm_split" "MPI_Comm_test_inter" "MPI_Intercomm_create"
    "MPI_Intercomm_merge"
-   ;;DerivedTypesRoutines
+   ;;Derived Types Routines
    "MPI_Type_commit" "MPI_Type_contiguous" "MPI_Type_extent*" "MPI_Type_free"
    "MPI_Type_hindexed*" "MPI_Type_hvector*" "MPI_Type_indexed" "MPI_Type_lb"
    "MPI_Type_size" "MPI_Type_struct*" "MPI_Type_ub*" "MPI_Type_vector"
-   ;;VirtualTopologyRoutines
+   ;;Virtual Topology Routines
    "MPI_Cart_coords" "MPI_Cart_create" "MPI_Cart_get" "MPI_Cart_map"
    "MPI_Cart_rank" "MPI_Cart_shift" "MPI_Cart_sub" "MPI_Cartdim_get"
    "MPI_Dims_create" "MPI_Graph_create" "MPI_Graph_get" "MPI_Graph_map"
    "MPI_Graph_neighbors" "MPI_Graph_neighbors_count" "MPI_Graphdims_get" "MPI_Topo_test"
-   ;;" "MiscellaneousRoutines
+   ;;Miscellaneous Routines
    "MPI_Address*" "MPI_Attr_delete*" "MPI_Attr_get*" "MPI_Attr_put*"
    "MPI_Keyval_create*" "MPI_Keyval_free*" "MPI_Pack" "MPI_Pack_size"
    "MPI_Pcontrol" "MPI_Unpack"
